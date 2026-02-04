6334e4503d4e0b0eacb2603608b6e9ddf7ce3385
Vetint02 <roy.park.0704@gmail.com>
2026-02-03 18:11:07 -0800

    middleware

--- server.js
+++ server.js
@@ -3,5 +3,11 @@ import usersRoute from "./routes/user.js";
 
 const app = express();
 
+//express.json() is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser.
+app.use(express.json());
+
+// if the user types /users, then go to user.js
+app.use("/users", usersRoute);
+// route-level middleware
 
 app.listen(3000, () => console.log("Server running on port 3000"));
